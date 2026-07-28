d47c42ab520e91ba9a7b2ddf5817e033599eddf7
leavihad001 <hleavit5@byu.edu>
2026-07-27 19:20:01 -0600

    Phase 5: 45(ish) minutes. Finished my facade tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -47,9 +47,7 @@ public class ServerFacadeTests {
 
         facade.register(request);
 
-        Exception exception = Assertions.assertThrows(Exception.class, () -> {
-            facade.register(request);
-        });
+        Exception exception = Assertions.assertThrows(Exception.class, () -> facade.register(request));
 
         Assertions.assertTrue(exception.getMessage().contains("403"));
     }
@@ -78,23 +76,135 @@ public class ServerFacadeTests {
 
         Assertions.assertDoesNotThrow(() -> facade.register(registerRequest));
 
-        Exception exception = Assertions.assertThrows(Exception.class, () -> {
-            facade.login(badLoginRequest);
-        });
+        Exception exception = Assertions.assertThrows(Exception.class, () -> facade.login(badLoginRequest));
 
         Assertions.assertTrue(exception.getMessage().contains("401"));
     }
-/*
-    @Test
-    @DisplayName("Test Name")
-    void testSuccess() {
-        RegisterRequest request = new RegisterRequest("testUser", "testPass", "test@test.com");
 
+    @Test
+    @DisplayName("Logout Success")
+    void logoutSuccess() {
+        RegisterRequest registerRequest = new RegisterRequest("testUser", "testPass", "test@test.com");
+        LoginRequest loginRequest = new LoginRequest("testUser", "testPass");
+
+        Assertions.assertDoesNotThrow(() -> {
+            facade.register(registerRequest);
+            LoginResult result = facade.login(loginRequest);
+            facade.logout(result.authToken());
+        });
     }
 
     @Test
-    @DisplayName("Test Name")
-    void testFail() throws Exception {
+    @DisplayName("Logout Fail - Bad AuthToken")
+    void logoutFail() {
 
-    }*/
+        Exception exception = Assertions.assertThrows(Exception.class, () -> facade.logout("bad-token"));
+
+        Assertions.assertTrue(exception.getMessage().contains("401"));
+    }
+
+    @Test
+    @DisplayName("Create Game Success")
+    void createGameSuccess() {
+        RegisterRequest registerRequest = new RegisterRequest("testUser", "testPass", "test@test.com");
+        LoginRequest loginRequest = new LoginRequest("testUser", "testPass");
+        CreateGameRequest createGameRequest = new CreateGameRequest("gameName");
+
+        Assertions.assertDoesNotThrow(() -> {
+            facade.register(registerRequest);
+            LoginResult result = facade.login(loginRequest);
+            facade.CreateGame(createGameRequest, result.authToken());
+        });
+    }
+
+    @Test
+    @DisplayName("Create Game Fail - Bad token")
+    void createGameFail() {
+        CreateGameRequest createGameRequest = new CreateGameRequest("gameName");
+
+        Exception exception = Assertions.assertThrows(Exception.class, () -> facade.CreateGame(createGameRequest, "bad-token"));
+
+        Assertions.assertTrue(exception.getMessage().contains("401"));
+    }
+
+    @Test
+    @DisplayName("List Games Success")
+    void listGamesSuccess() {
+        RegisterRequest registerRequest = new RegisterRequest("testUser", "testPass", "test@test.com");
+        LoginRequest loginRequest = new LoginRequest("testUser", "testPass");
+        CreateGameRequest createGameRequest = new CreateGameRequest("gameName");
+
+        String validToken = Assertions.assertDoesNotThrow(() -> {
+            facade.register(registerRequest);
+            LoginResult result = facade.login(loginRequest);
+            facade.CreateGame(createGameRequest, result.authToken());
+
+            return result.authToken();
+        });
+
+        Assertions.assertDoesNotThrow(() -> {
+            facade.listGames(validToken);
+        });
+    }
+
+    @Test
+    @DisplayName("List Games Fail - Bad Token")
+    void listGamesFail() {
+        RegisterRequest registerRequest = new RegisterRequest("testUser", "testPass", "test@test.com");
+        LoginRequest loginRequest = new LoginRequest("testUser", "testPass");
+        CreateGameRequest createGameRequest = new CreateGameRequest("gameName");
+
+        Assertions.assertDoesNotThrow(() -> {
+            facade.register(registerRequest);
+            LoginResult result = facade.login(loginRequest);
+            facade.CreateGame(createGameRequest, result.authToken());
+        });
+
+        Exception exception = Assertions.assertThrows(Exception.class, () -> facade.listGames("bad-token"));
+
+        Assertions.assertTrue(exception.getMessage().contains("401"));
+    }
+
+    @Test
+    @DisplayName("Join Game Success")
+    void JoinGameSuccess() {
+        RegisterRequest registerRequest = new RegisterRequest("testUser", "testPass", "test@test.com");
+        LoginRequest loginRequest = new LoginRequest("testUser", "testPass");
+        CreateGameRequest createGameRequest = new CreateGameRequest("gameName");
+
+        LoginResult loginResult = Assertions.assertDoesNotThrow(() -> {
+            facade.register(registerRequest);
+            return facade.login(loginRequest);
+        });
+
+        JoinGameRequest joinGameRequest = Assertions.assertDoesNotThrow(() -> {
+            CreateGameResult createResult = facade.CreateGame(createGameRequest, loginResult.authToken());
+            return new JoinGameRequest("WHITE", createResult.gameID());
+        });
+
+        Assertions.assertDoesNotThrow(() -> facade.joinGame(joinGameRequest, loginResult.authToken()));
+    }
+
+    @Test
+    @DisplayName("Join Game Fail - Bad Game ID")
+    void joinGameFail() {
+        RegisterRequest registerRequest = new RegisterRequest("testUser", "testPass", "test@test.com");
+        LoginRequest loginRequest = new LoginRequest("testUser", "testPass");
+        CreateGameRequest createGameRequest = new CreateGameRequest("gameName");
+
+        String validToken = Assertions.assertDoesNotThrow(() -> {
+            facade.register(registerRequest);
+            LoginResult loginResult = facade.login(loginRequest);
+            facade.CreateGame(createGameRequest, loginResult.authToken());
+
+            return loginResult.authToken();
+        });
+        
+        Exception exception = Assertions.assertThrows(Exception.class, () -> {
+            JoinGameRequest joinGameRequest = new JoinGameRequest("WHITE", 9999);
+            facade.joinGame(joinGameRequest, validToken);
+        });
+
+        Assertions.assertTrue(exception.getMessage().contains("400"));
+    }
 }
